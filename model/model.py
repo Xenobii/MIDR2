fixed masking

model/model.py
@@ -104,12 +104,11 @@ class SNAModel(nn.Module):
         self.len_padding = config['input']['len_padding']
         self.npitch      = config['midi']['num_notes']
 
-        self.center_scale = torch.tensor([
+        self.register_buffer('center_scale', torch.tensor([
             config['midr']['spiral']['radius'],  # x
             config['midr']['spiral']['radius'],  # y
             config['midr']['spiral']['height']   # z
-        ], dtype=torch.float32)
-
+        ], dtype=torch.float32))
         self.layernorm = nn.LayerNorm(normalized_shape=self.nbin)
 
         self.conv1d = nn.Sequential(
@@ -162,7 +161,13 @@ class SNAModel(nn.Module):
         )
 
         self.fc_center = nn.Sequential(
-            nn.Linear(3 * self.nbin, 88),
+            nn.Linear(self.nbin, 88),
+            nn.LeakyReLU(inplace=True),
+            nn.Linear(88, 256),
+            nn.LeakyReLU(inplace=True),
+            nn.Linear(256, 512),
+            nn.LeakyReLU(inplace=True),
+            nn.Linear(512, 88),
             nn.LeakyReLU(inplace=True),
             nn.Linear(88, 12),
             nn.LeakyReLU(inplace=True),
@@ -171,7 +176,11 @@ class SNAModel(nn.Module):
         )
 
         self.fc_diam = nn.Sequential(
-            nn.Linear(3 * self.nbin, 88),
+            nn.Linear(self.nbin, 88),
+            nn.LeakyReLU(inplace=True),
+            nn.Linear(88, 256),
+            nn.LeakyReLU(inplace=True),
+            nn.Linear(256, 88),
             nn.LeakyReLU(inplace=True),
             nn.Linear(88, 12),
             nn.LeakyReLU(inplace=True),
@@ -190,11 +199,7 @@ class SNAModel(nn.Module):
         x = self.conv1d(x)
         # [B*nframe, nbin]
         x = self.conv1darray(x).squeeze(1)
-        # [B*nframe, 1, nbin]
-        x = self.conv2spiral(x).permute(0, 2, 1)
-        # [B*nframe, 3, nbin]
-        x = self.flatten(x)
-        # [B*nframe, 3*nbin]
+        # [B*nframe, nbin]
         spiral_cd = self.fc_diam(x)
         # [B*nframe, 1]
         spiral_cc = self.fc_center(x)
@@ -232,7 +237,7 @@ class DilatedSNAModel(nn.Module):
                 padding=2,
                 stride=1
             ),
-            nn.LeakyReLU(),
+            nn.LeakyReLU(inplace=True),
         )
 
         self.dilconv = HarmonicDilation(self.nbin)
@@ -245,7 +250,7 @@ class DilatedSNAModel(nn.Module):
                 padding=5,
                 stride=3
             ),
-            nn.LeakyReLU()
+            nn.LeakyReLU(inplace=True)
         )
 
         self.conv2spiral = SpiralTransform(
@@ -256,19 +261,19 @@ class DilatedSNAModel(nn.Module):
         self.flatten = nn.Flatten(start_dim=1)
 
         self.fc_center = nn.Sequential(
-            nn.Linear(88*3, 88),
-            nn.LeakyReLU(),
+            nn.Linear(88, 88),
+            nn.LeakyReLU(inplace=True),
             nn.Linear(88, 12),
-            nn.LeakyReLU(),
+            nn.LeakyReLU(inplace=True),
             nn.Linear(12, 3),
             nn.Tanh()
         )
 
         self.fc_diam = nn.Sequential(
-            nn.Linear(88*3, 88),
-            nn.LeakyReLU(),
+            nn.Linear(88, 88),
+            nn.LeakyReLU(inplace=True),
             nn.Linear(88, 12),
-            nn.LeakyReLU(),
+            nn.LeakyReLU(inplace=True),
             nn.Linear(12, 1),
             nn.ReLU()
         )
@@ -287,10 +292,6 @@ class DilatedSNAModel(nn.Module):
         # [B*nframe, 20, 264]
         x = self.conv1d2(x).squeeze(1)
         # [B*nframe, 88]
-        x = self.conv2spiral(x).permute(0, 2, 1)
-        # [B*nframe, 3, 88]
-        x = self.flatten(x)
-        # [B*nframe, 3*88]
         spiral_cd = self.fc_diam(x)
         # [B*nframe, 1]
         spiral_cc = self.fc_center(x)
@@ -309,15 +310,30 @@ class HarmonicDilation(nn.Module):
 
         c_in  = 10
         c_out = 20
-        dilations = [36, 54, 72, 84, 92, 101, 108, 114]
+        # dilations = [36, 54, 72, 84, 92, 101, 108, 114]
+        dilations = [36, 54, 72]
 
         self.convs = nn.ModuleList([
             nn.Conv1d(c_in, c_out, 3, padding='same', dilation=d) for d in dilations
         ])
+        # self.conv_1 = nn.Conv1d(c_in, c_out, 3, padding='same', dilation=dilations[0])
+        # self.conv_2 = nn.Conv1d(c_in, c_out, 3, padding='same', dilation=dilations[1])
+        # self.conv_3 = nn.Conv1d(c_in, c_out, 3, padding='same', dilation=dilations[2])
+        # self.conv_4 = nn.Conv1d(c_in, c_out, 3, padding='same', dilation=dilations[3])
+        # self.conv_5 = nn.Conv1d(c_in, c_out, 3, padding='same', dilation=dilations[4])
+        # self.conv_6 = nn.Conv1d(c_in, c_out, 3, padding='same', dilation=dilations[5])
+        # self.conv_7 = nn.Conv1d(c_in, c_out, 3, padding='same', dilation=dilations[6])
+        # self.conv_8 = nn.Conv1d(c_in, c_out, 3, padding='same', dilation=dilations[7])
+
         self.relu = nn.ReLU()
 
     def forward(self, x):
-        return self.relu(sum(conv(x) for conv in self.convs))
+        x = torch.stack([conv(x) for conv in self.convs], dim=0)
+        x = x.sum(dim=0)
+        # x = self.conv_1(x) + self.conv_2(x) + self.conv_3(x) + self.conv_4(x) +\
+        #     self.conv_5(x) + self.conv_6(x) + self.conv_7(x) + self.conv_8(x)
+        x = self.relu(x)
+        return x
 
     
 
@@ -350,7 +366,7 @@ class SpiralTransform(nn.Module):
     def forward(self, x: torch.Tensor):
         """
         x: [B, F]
-        returns: [B, F, 3]  # weighted by the input amplitude
+        returns: [B, F, 3]
         """
         B, F = x.shape
         if F != self.freq_bins:

train/plot_metrics.py
@@ -12,7 +12,7 @@ def plot_loss():
     valid_loss_cd = []
     valid_loss_cc = []
     
-    for path in sorted(glob.glob('model/checkpoints/checkpoint_*.pth')):
+    for path in sorted(glob.glob('model/checkpoints2/checkpoint_*.pth')):
         checkpoint = torch.load(path, map_location='cpu', weights_only=False)
         train_loss.append(checkpoint['epoch_loss_train'])
         train_loss_cd.append(checkpoint['epoch_loss_train_cd'])

train/train.py
@@ -35,7 +35,7 @@ class Trainer():
         print(f"Training on {self.device}")
 
         # Model settings
-        self.model = DilatedSNAModel(config)
+        self.model = SNAModel(config)
         self.model = self.model.to(self.device)
         print(f"Trainable model parameters: {sum(p.numel() for p in self.model.parameters() if p.requires_grad)}")
         # def count_parameters(model):
@@ -56,8 +56,8 @@ class Trainer():
         self.scheduler = optim.lr_scheduler.ReduceLROnPlateau(self.optimizer)
         self.scaler    = GradScaler(enabled=(self.device == 'cuda'))
 
-        self.criterion_spiral_cc = nn.L1Loss(reduction='none')
-        self.criterion_spiral_cd = nn.L1Loss(reduction='none')
+        self.criterion_spiral_cc = nn.HuberLoss(reduction='none')
+        self.criterion_spiral_cd = nn.HuberLoss(reduction='none')
 
         # Dataset loading
         f_dataset = 'dataset/processed_dataset.h5'
@@ -83,7 +83,7 @@ class Trainer():
         print(f"Initialized trainer in {(t1 - t0):.3f} seconds")
 
     def train(self):
-        os.makedirs("model/checkpoints_wrapped", exist_ok=True)
+        os.makedirs("model/checkpoints2", exist_ok=True)
         for epoch in range(self.epochs):
             print(f"-- Epoch {epoch} --")
             t0 = time.time()
@@ -109,8 +109,8 @@ class Trainer():
                 'epoch_loss_valid_cc': epoch_loss_valid_cc,
                 'optimizer_dict'     : self.optimizer.state_dict(),
                 'model_dict'         : self.model.state_dict()
-            }, f'model/checkpoints/checkpoint_{epoch}.pth')
-            print(f"Checkpoint saved at: model/checkpoints_wrapped/checkpoint_{epoch}.pth")
+            }, f'model/checkpoints2/checkpoint_{epoch}.pth')
+            print(f"Checkpoint saved at: model/checkpoints2/checkpoint_{epoch}.pth")
 
             self.scheduler.step(epoch_loss_valid)
 
@@ -194,8 +194,10 @@ class Trainer():
                 loss_spiral_cc = self.criterion_spiral_cc(label_spiral_cc, output_spiral_cc)
 
                 # Mask
-                loss_spiral_cd = (loss_spiral_cd * note_mask).sum() / (note_mask.sum() + eps)
-                loss_spiral_cc = (loss_spiral_cc * note_mask).sum() / (note_mask.sum() + eps)
+                mask_cd = note_mask.expand_as(loss_spiral_cd)
+                mask_cc = note_mask.expand_as(loss_spiral_cc)
+                loss_spiral_cd = (loss_spiral_cd * mask_cd).sum() / (mask_cd.sum() + eps)
+                loss_spiral_cc = (loss_spiral_cc * mask_cc).sum() / (mask_cc.sum() + eps)
                 
                 loss = loss_spiral_cd + loss_spiral_cc
 